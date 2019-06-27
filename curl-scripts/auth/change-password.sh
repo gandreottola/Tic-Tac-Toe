42aa84813d10118fdd97302b1f27707e692e2ed5
gandreottola <gandreottola09@gmail.com>
2019-06-27 13:02:47 -0400

    Finished authentication for user
    completed authentication for sign-up
    completed authentication for sign-in
    completed authentication for sign-out
    completed authentication for change password

---
+++ curl-scripts/auth/change-password.sh
@@ -0,0 +1,17 @@
+# VARIABLE=VALUE sh curl-scripts/auth/change-password.sh
+
+#!/bin/bash
+
+curl "https://tic-tac-toe-wdi.herokuapp.com/change-password" \
+  --include \
+  --request PATCH \
+  --header "Authorization: Token token=${TOKEN}" \
+  --header "Content-Type: application/json" \
+  --data '{
+    "passwords": {
+      "old": "'"${OLD}"'",
+      "new": "'"${NEW}"'"
+    }
+  }'
+
+echo
