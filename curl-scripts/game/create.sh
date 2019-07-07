0c92a87dfd486b55d6c6123320e5842877a54eef
gandreottola <gandreottola09@gmail.com>
2019-07-07 16:09:11 -0400

    Tested update and create curl scripts
    test is successful

--- curl-scripts/game/create.sh
+++ curl-scripts/game/create.sh
@@ -1,21 +1,21 @@
 # VARIABLE=VALUE sh curl-scripts/game/create.sh
-
-curl "https://tic-tac-toe-wdi.herokuapp.com/create" \
+curl "https://tic-tac-toe-wdi.herokuapp.com/games" \
 --include \
 --request POST \
 --header "Authorization: Token token=${TOKEN}" \
 --header "Content-Type: application/json" \
 --data '{
-  "game": {
-    "id": "'"${ID}"'",
-    "cells": [" "," "," "," "," "," "," "," "," "],
-    "over": false,
-    "player_x": {
-      "id": "'"${ID}"'",
-      "email": "'"${EMAIL}"'"
-    },
-    "player_o": null
-  }
-}'
+        "game": {
+          "id": "'"${ID}"'",
+          "cells":["","","","","","","","",""],
+          "over": false,
+          "player_x": {
+              "id": 1,
+              "email": "'"${EMAIL}"'"
+            },
+            "player_o": null
+        }
+  }'
+
 
 echo
